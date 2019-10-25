d6eaf66ccf7036fc56cf2cf33a2bb2923dd4e9c0
Budafly <info@budafly.com>
2019-10-25 14:03:29 -0500

    Update MainActivity.java

--- native/android/src/MainActivity.java
+++ native/android/src/MainActivity.java
@@ -3,37 +3,24 @@ package com.adobe.phonegap.appbuda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	static int counter = 0;
-	TextView tv;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		tv = (TextView) findViewById(R.id.count);
-		tv.setText("Counter : " + String.valueOf(counter));
+	int _counter = 0;
+	
+	public MainActivity() {}
+	
+	public int getCounter() {		
+		return _counter;
 	}
-
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
-			tv.setText("Counter : " + String.valueOf(++counter));
-			Toast.makeText(this, "Volume Down Pressed", Toast.LENGTH_SHORT)
-			.show();
-			return true;
-		}
+	
+	public int setCounter(int n) {
+		_counter = n;
+		return _counter;
+	}
+		
+	public void onKeyDown(int keyCode, KeyEvent event) {		
 		if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-			tv.setText("Counter : " + String.valueOf(–counter));
-			Toast.makeText(this, "Volume Up Pressed", Toast.LENGTH_SHORT)
-			.show();
-			return true;
-		}
-
-		else {
-			return super.onKeyDown(keyCode, event);
+			++_counter;
 		}
 	}
 }
